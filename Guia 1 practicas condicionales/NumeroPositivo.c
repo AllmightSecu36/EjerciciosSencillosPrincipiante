4ad065f9889caa55420eacd197a592374279e26a
AllmightSecu36 <torrez.ag468@gmail.com>
2026-03-29 15:57:03 -0400

    Create NumeroPositivo.c

---
+++ Guia 1 practicas condicionales/NumeroPositivo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    int numero;
+printf ("Dame un numero entero \n");
+scanf ("%d",&numero);
+
+if (numero>0) 
+    printf("El numero es positivo \n");
+    else
+ 
+    printf("El numero es negativo \n");
+
+return 0;
+}
