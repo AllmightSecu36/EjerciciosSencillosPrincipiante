601d52663369d9690bcae190eb9620b0aec41330
AllmightSecu36 <torrez.ag468@gmail.com>
2026-04-09 17:23:51 -0400

    trabajando con funciones y punteros
    Enunciado: Sistema de Gestión de Inventario Tecnológico
    Vas a desarrollar un programa que permita registrar equipos de computación (puedes imaginar que es para un laboratorio o una oficina). El programa debe definir una estructura (struct) y gestionar la entrada de datos de forma limpia.
    
    1. Definición de la Estructura
    Crea un struct llamado Equipo que contenga:
    
    Nombre del equipo: (Cadena de caracteres, debe permitir espacios, ej: "Monitor Samsung 24 pulgadas").
    
    Código de inventario: (Entero).
    
    Precio: (Decimal/Float).
    
    2. Requerimientos del Programa
    El programa debe preguntar cuántos equipos se van a registrar y luego, para cada uno:
    
    Leer el Nombre con fgets: Asegúrate de usar stdin.
    
    Leer el Código y el Precio con scanf: Aquí es donde los datos numéricos entran en juego.
    
    Gestionar el Buffer: Después de usar scanf, deberás usar getchar() para "limpiar" el salto de línea que queda en el buffer antes de la siguiente vuelta del ciclo, de lo contrario, el próximo fgets se saltará.
    
    3. Salida de Datos
    Al final, el programa debe mostrar una tabla o un listado bien organizado con todos los equipos registrados y el valor total del inventario (la suma de todos los precios).

---
+++ Ejercicios simples de programacion/intercambiodenotas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void intercambiar (int *a, int *b){
+    int aux;
+    aux=*a;
+    *a=*b;
+    *b=aux;
+}
+int main (){
+    int nota1,nota2;
+    printf ("ingrese sus 2 notas \n");
+    scanf ("%d %d",&nota1,&nota2);
+    printf("Tu primera nota es: %d  Tu segunda nota es: %d \n",nota1,nota2);
+    intercambiar (&nota1,&nota2);
+    printf("VALORES FINALES -> Nota 1: %d, Nota 2: %d\n", nota1, nota2);
+}
