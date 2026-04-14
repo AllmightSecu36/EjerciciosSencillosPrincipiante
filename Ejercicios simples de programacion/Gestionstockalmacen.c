f8e77e5bf8593f1865039ba1719e386b9689de96
AllmightSecu36 <torrez.ag468@gmail.com>
2026-04-14 14:44:36 -0400

    Update Gestionstockalmacen.c

--- Ejercicios simples de programacion/Gestionstockalmacen.c
+++ Ejercicios simples de programacion/Gestionstockalmacen.c
@@ -37,7 +37,7 @@ int main (){
             return 0;
             break;        
         default:
-        printf ("Ingrese una de las 4 opciones\n");
+        printf ("Ingrese una de las 4 opciones...\n");
             break;
         }
         numoperaciones+=1;
