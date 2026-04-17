594c36b33e09da029f0c104c6f7fec613a28171b
AllmightSecu36 <torrez.ag468@gmail.com>
2026-04-17 14:26:40 -0400

    Update VerideNumeros.c

--- Ejercicios simples de programacion/VerideNumeros.c
+++ Ejercicios simples de programacion/VerideNumeros.c
@@ -4,8 +4,8 @@ int num1, num2;
 printf ("Dame dos numeros enteros \n");
 scanf ("%d %d",&num1,&num2);
 if (num1>10 && num2>10)
-    printf ("Ambos numeros son mayores que 10 \n");
+    printf ("Ambos numeros son mayores que 10. \n");
 else  
-    printf ("Al menos uno de los numeros no es mayor que 10 \n");
+    printf ("Al menos uno de los numeros no es mayor que 10. \n");
 return 0;
 }  
