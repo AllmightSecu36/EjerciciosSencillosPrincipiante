4aa8a339ba2ab014b31ae07a4b03a950cd3122ae
AllmightSecu36 <torrez.ag468@gmail.com>
2026-04-22 14:19:19 -0400

    Update Examen.c

--- Ejercicios simples de programacion/Examen.c
+++ Ejercicios simples de programacion/Examen.c
@@ -7,8 +7,8 @@ int main (){
     scanf ("%f %f %f",&n1,&n2,&n3);
     while (opcion!=3){
         printf ("--Menu de opciones--\n");
-        printf ("1. Promedio de las notas\n");
-        printf ("2. Nota con mayor valor\n");
+        printf ("1. Promedio de las notas.\n");
+        printf ("2. Nota con mayor valor.\n");
         printf ("3. Salir del sistema...\n");
         scanf ("%d",&opcion);
         switch (opcion) {
